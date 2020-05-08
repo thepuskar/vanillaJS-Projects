0877ca124f1cf14ec5988f30d1b4dda4504a7287
devpuskar <puskar.ad@gmail.com>
2020-05-09 02:08:24 +0545

    value can be increase, decrease and reset and style has been added

---
+++ 02-simpleCounter/index.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
