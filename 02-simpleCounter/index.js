d87889ad2d07642211c53788efd174e57b10fec7
devpuskar <puskar.ad@gmail.com>
2020-05-09 02:23:21 +0545

    style fixed

--- 02-simpleCounter/index.js
+++ 02-simpleCounter/index.js
@@ -14,6 +14,14 @@ btns.forEach(function (btn) {
     } else {
       count = 0;
     }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
+    }
     value.textContent = count;
   });
 });
