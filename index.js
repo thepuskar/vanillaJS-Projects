c924b1e8fe4051e8471dcc8a227f542d2f299ef3
devpuskar <puskar.ad@gmail.com>
2020-05-09 18:26:22 +0545

    navbar added to main home

--- index.js
+++ index.js
@@ -52,3 +52,11 @@ list.forEach((result) => {
 
   container.innerHTML += content;
 });
+
+//navbar
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
