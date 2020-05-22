14b61c02d95457350278f530ae067b64565f2163
devpuskar <puskar.ad@gmail.com>
2020-05-22 17:09:47 +0545

    Saved High Scores in Local Storage

--- quize/js/end.js
+++ quize/js/end.js
@@ -2,6 +2,11 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -11,4 +16,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = (e) => {
   console.log("clicked the save button!");
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/quize");
 };
