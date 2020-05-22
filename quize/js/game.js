14b6338bd0f2b59c11dcbe8a4233b4d1a0f3bc7a
devpuskar <puskar.ad@gmail.com>
2020-05-22 18:35:21 +0545

    Fetch API to Load Local Questions

--- quize/js/game.js
+++ quize/js/game.js
@@ -10,33 +10,18 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestion = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3,
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4,
-  },
-];
+let questions = [];
+fetch("question.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    questions = loadedQuestions;
+    startGame();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
@@ -98,5 +83,3 @@ incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
-
-startGame();
