99f90e0c700ed2daef922bf091ccb48a02ef82ed
devpuskar <puskar.ad@gmail.com>
2020-05-22 19:15:59 +0545

    Create a Spinning Loader

--- quize/js/game.js
+++ quize/js/game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 let currentQuestion = {};
 let acceptingAnswer = false;
@@ -37,6 +39,8 @@ fetch(
 
       return formattedQuestion;
     });
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
     startGame();
   })
   .catch((err) => {
