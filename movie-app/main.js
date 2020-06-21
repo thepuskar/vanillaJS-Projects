f5abf2176dd9f7d60fad6c263983fcc2db4e30d4
devpuskar <puskar.ad@gmail.com>
2020-06-21 17:24:38 +0545

    display the movie poster after search

--- movie-app/main.js
+++ movie-app/main.js
@@ -1,7 +1,43 @@
-const API_KEY = "f9cde82c44c9ed80078efb7e0e8e1bc9";
-
 const buttonElement = document.querySelector("#search");
 const inputElement = document.querySelector("#inputValue");
+const movieSerchList = document.querySelector("#movieSerchList");
+
+const API_KEY = "f9cde82c44c9ed80078efb7e0e8e1bc9";
+const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+function movieImg(movies) {
+  return movies.map((movie) => {
+    if (movie.poster_path) {
+      return `
+        <div class="img"> 
+        <img src=${IMG_URL + movie.poster_path} data-movie-id=${movie.id} />
+        </div>`;
+    }
+  });
+}
+
+function createMovieContainer(movies) {
+  const movieElement = document.createElement("div");
+  movieElement.setAttribute("class", "movie-container");
+  const movieTemplate = `
+        <div>
+            <div class="movie_card">
+                ${movieImg(movies)}
+            </div>
+        </div>
+  `;
+
+  movieElement.innerHTML = movieTemplate;
+  return movieElement;
+}
+
+function renderSearchMovies(data) {
+  movieSerchList.innerHTML = "";
+  const movies = data.results;
+  const movieBlock = createMovieContainer(movies);
+  movieSerchList.appendChild(movieBlock);
+  console.log("data = ", data);
+}
 
 buttonElement.onclick = function (event) {
   event.preventDefault();
@@ -14,10 +50,10 @@ buttonElement.onclick = function (event) {
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => {
-      console.log("data = ", data);
-    })
+    .then(renderSearchMovies)
     .catch((error) => {
       console.log("Error");
     });
+
+  inputElement.value = "";
 };
