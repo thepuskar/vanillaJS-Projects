aa467047e8de70d429cc4466474ce7bf7bd281e7
devpuskar <puskar.ad@gmail.com>
2020-06-21 14:46:24 +0545

    getted api key and started searching movie based on the user input

--- movie-app/main.js
+++ movie-app/main.js
@@ -1,8 +1,23 @@
+const API_KEY = "f9cde82c44c9ed80078efb7e0e8e1bc9";
+
 const buttonElement = document.querySelector("#search");
 const inputElement = document.querySelector("#inputValue");
 
 buttonElement.onclick = function (event) {
   event.preventDefault();
   const value = inputElement.value;
-  console.log("Value = " + value);
+  const url =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    API_KEY +
+    "&query=" +
+    value;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("data = ", data);
+    })
+    .catch((error) => {
+      console.log("Error");
+    });
 };
